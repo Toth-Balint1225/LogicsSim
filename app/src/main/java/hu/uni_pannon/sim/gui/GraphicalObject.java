72a3169c84913bfddff9cbbc85c1b1a63671e744
Toth-Balint1225 <74766154+Toth-Balint1225@users.noreply.github.com>
2022-04-28 11:55:39 +0200

    Improved pins

--- app/src/main/java/hu/uni_pannon/sim/gui/GraphicalObject.java
+++ app/src/main/java/hu/uni_pannon/sim/gui/GraphicalObject.java
@@ -1,16 +1,19 @@
 package hu.uni_pannon.sim.gui;
 
 
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 import hu.uni_pannon.sim.logic.Component;
+import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.Group;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
 public class GraphicalObject extends Group {
@@ -23,6 +26,7 @@ public class GraphicalObject extends Group {
     private DoubleProperty yProperty;
 
     private String id;
+    private String name;
     private MainView controller;
 
     private Pos lastPos;
@@ -32,6 +36,11 @@ public class GraphicalObject extends Group {
 
     boolean interacted = false;
 
+    private enum PinDirection {
+        RIGHT,
+        LEFT
+    }
+
     // "id-connecting" the pins to the component's inputs and outputs
     public class Pin extends Group {
         private DoubleProperty xProperty;
@@ -39,6 +48,9 @@ public class GraphicalObject extends Group {
 
         private String id;
         private Circle c;
+        private Line l;
+
+        private final int len = 10;
 
         public Pin(String id) {
             xProperty = new SimpleDoubleProperty();
@@ -46,10 +58,16 @@ public class GraphicalObject extends Group {
 
             this.id = id;
             c = new Circle();
-            c.setRadius(10);
+            c.setRadius(8);
             c.centerXProperty().bind(xProperty);
             c.centerYProperty().bind(yProperty);
-            getChildren().add(c);
+
+            l = new Line();
+            l.startYProperty().bind(yProperty);
+            l.endYProperty().bind(yProperty);
+            l.setStrokeWidth(2);
+
+            getChildren().addAll(c,l);
 
 
             addEventFilter(MouseEvent.MOUSE_PRESSED,
@@ -58,6 +76,23 @@ public class GraphicalObject extends Group {
                     GraphicalObject.this.activePinId = this.id;
                     GraphicalObject.this.controller.notifyPress(GraphicalObject.this.id,evt);
                 });
+        }
+
+        public void anchorToObject(DoubleBinding x, DoubleBinding y, PinDirection direction) {
+            switch (direction) {
+            case LEFT:
+                xProperty.bind(x.subtract(len));
+                yProperty.bind(y);
+                l.startXProperty().bind(xProperty);
+                l.endXProperty().bind(xProperty.add(len));
+                break;
+            case RIGHT:
+                xProperty.bind(x.add(len));
+                yProperty.bind(y);
+                l.startXProperty().bind(xProperty);
+                l.endXProperty().bind(xProperty.subtract(len));
+                break;
+            }
         }
 
         public DoubleProperty xProperty() {
@@ -73,6 +108,10 @@ public class GraphicalObject extends Group {
 
     private String activePinId;
 
+    private DoubleProperty widthProperty;
+    private DoubleProperty heightProperty;
+    private Group body;
+
     public GraphicalObject(String id, MainView controller) {
         this.id = id;
         this.controller = controller;
@@ -80,42 +119,68 @@ public class GraphicalObject extends Group {
 
     public GraphicalObject(String id, MainView controller, double x, double y) {
         this(id,controller);
+        name = "CMP";
         lastPos = new Pos();
         pins = new TreeMap<>();
         activePinId = null;
         xProperty = new SimpleDoubleProperty(x);
         yProperty = new SimpleDoubleProperty(y);
+        heightProperty = new SimpleDoubleProperty(100);
+        widthProperty = new SimpleDoubleProperty(50);
+
+        body = new Group();
         // debug
             rect = new Rectangle();
-            rect.setWidth(50);
-            rect.setHeight(80);
-            rect.xProperty().bind(xProperty.subtract(25));
-            rect.yProperty().bind(yProperty.subtract(40));
+            // rect.setFill(Color.TRANSPARENT);
+            rect.widthProperty().bind(widthProperty);
+            rect.heightProperty().bind(heightProperty);
+            rect.xProperty().bind(xProperty.subtract(widthProperty.divide(2.0)));
+            rect.yProperty().bind(yProperty.subtract(heightProperty.divide(2.0)));
             getChildren().addAll(rect);
         addEventFilters();
         addPins();
     }
 
     public void addPins() {
-        int i = 1;
-        for (String pinId : controller.getModel().getComponentById(id).getLUT().inputs()) {
-            Pin p = new Pin(pinId);
-            p.xProperty.bind(xProperty.subtract(35));
-            p.yProperty.bind(yProperty.add(Math.pow(-1,i) * 15));
-            pins.put(pinId,p);
+
+        List<String> inputs = controller.getModel().getComponentById(id).getLUT().inputs();
+        List<String> outputs = controller.getModel().getComponentById(id).getLUT().outputs();
+
+        if (inputs.size() == 1) {
+            Pin p = new Pin(inputs.get(0));
+            p.anchorToObject(xProperty.subtract(widthProperty.divide(2.0)), yProperty.multiply(1.0), PinDirection.LEFT);
+            pins.put(inputs.get(0),p);
             getChildren().add(p);
-            i++;
+        } else {
+            int i = 1;
+            for (String pinId : inputs) {
+                Pin p = new Pin(pinId);
+                p.anchorToObject(xProperty.subtract(widthProperty.divide(2.0)), yProperty.add(Math.pow(-1,i) * heightProperty.divide(4.0).doubleValue()), PinDirection.LEFT);
+                pins.put(pinId,p);
+                getChildren().add(p);
+                i++;
+            }
         }
 
-        i = 1;
-        for (String pinId : controller.getModel().getComponentById(id).getLUT().outputs()) {
-            Pin p = new Pin(pinId);
-            p.xProperty.bind(xProperty.add(35));
-            p.yProperty.bind(yProperty.add(Math.pow(-1,i) * 15));
-            pins.put(pinId,p);
+        if (outputs.size() == 1) {
+            Pin p = new Pin(outputs.get(0));
+            p.anchorToObject(xProperty.add(widthProperty.divide(2.0)), yProperty.multiply(1.0), PinDirection.RIGHT);
+            pins.put(outputs.get(0),p);
             getChildren().add(p);
-            i++;
+        } else {
+            int i = 1;
+            for (String pinId : outputs) {
+                Pin p = new Pin(pinId);
+                p.anchorToObject(xProperty.add(widthProperty.divide(2.0)), yProperty.add(Math.pow(-1,i) * heightProperty.divide(4.0).doubleValue()), PinDirection.RIGHT);
+                pins.put(pinId,p);
+                getChildren().add(p);
+                i++;
+            }
         }
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public boolean isPinActive() {
